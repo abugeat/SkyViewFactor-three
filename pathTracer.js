66b5f5b3d9b76261bfe4f46a72f3be97602cb85a
abugeat <45351626+abugeat@users.noreply.github.com>
2022-08-06 15:45:23 +0200

    Some cleaning and settings.

--- pathTracer.js
+++ pathTracer.js
@@ -34,6 +34,7 @@ function init() {
 	renderer.setClearColor( 0x09141a );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	// https://stackoverflow.com/questions/69345609/three-js-textures-become-grayish-with-renderer-outputencoding-srgbencoding
+	// https://learnopengl.com/Advanced-Lighting/Gamma-Correction
 	// renderer.outputEncoding = THREE.sRGBEncoding;
 	renderer.outputEncoding = THREE.LinearEncoding; 
 	document.body.appendChild( renderer.domElement );
@@ -122,36 +123,12 @@ function init() {
 				vec3 newvec;
 			    
 				if (v.z<v.x)
-				{
-					newvec = normalize(vec3(v.y,-v.x,0.0));
-				}
-				
+				{newvec = normalize(vec3(v.y,-v.x,0.0));}
 				else
-				{
-					newvec = normalize(vec3(0.0,-v.z,v.y));
-				}
-				
+				{newvec = normalize(vec3(0.0,-v.z,v.y));}
+		
 				return newvec;
 			}
-
-			// // random function
-			// float random(vec2 xy)
-			// {
-			// 	return fract(sin(dot(xy,vec2(12.9898,78.233)))*43758.5453123);
-			// }
-
-			// // return a perpendicular vector
-			// vec3 perpvec(vec3 v) 
-			// {
-			// 	vec3 newvec;
-			    
-			// 	if (v.z<v.x)
-			// 	{newvec = vec3(v.y,-v.x,0.0);}
-			// 	else
-			// 	{newvec = vec3(0.0,-v.z,v.y);}
-				
-			// 	return newvec;
-			// }
 
 			vec4 getQuaternion(vec3 ax, float angle)
 			{
@@ -175,7 +152,6 @@ function init() {
 				return rotated;
 			}
 
-			
 			vec3 viewFactorDirectionGenerator(vec3 normal, vec2 randpara)
 			{	
 				// get random angle (with cos rule)
@@ -205,7 +181,7 @@ function init() {
 				gl_FragColor = vec4( 0.0 );
 
 				vec3 throughputColor = vec3( .0 );
-				vec3 randomPoint = vec3( .0 );
+				// vec3 randomPoint = vec3( .0 );
 
 				// hit results
 				uvec4 faceIndices = uvec4( 0u );
@@ -214,82 +190,22 @@ function init() {
 				float side = 1.0;
 				float dist = 0.0;
 
-				// for ( int i = 0; i < BOUNCES; i ++ ) {
 				for ( int i = 0; i <= 1; i ++ ) { // Correspond to 0 reflexion, 2 rays: 1 for camera view and 1 for pixel color
 
 					if ( ! bvhIntersectFirstHit( bvh, rayOrigin, rayDirection, faceIndices, faceNormal, barycoord, side, dist ) ) {
 
-						// float value = ( rayDirection.y + 0.5 ) / 1.5;
-						// vec3 skyColor = mix( vec3( 1.0 ), vec3( 1.0, 1.0, 1.0 ), value );
 						vec3 skyColor = vec3(1.0, 1.0, 1.0);
 
-						// gl_FragColor = vec4(throughputColor, 1.0 );
 						if (i == 1 && rayDirection.y > 0.0 ){
 							gl_FragColor = vec4(1.0, 1.0, 1.0 , 1.0 );
 						} else {
 							gl_FragColor = vec4(0.0, 0.0, 0.0 , 1.0 );
 						}
-
-						// gl_FragColor.rgb = pow(gl_FragColor.rgb, vec3(2.2));
 						// https://learnopengl.com/Advanced-Lighting/Gamma-Correction
 						
-						// gl_FragColor = vec4(
-						// 	normalize(vec3(
-						// 		random(vec2(faceNormal.x,faceNormal.y)),
-						// 		random(vec2(faceNormal.x,faceNormal.z)),
-						// 		random(vec2(faceNormal.y,faceNormal.z)))),
-						// 		1.0);
-						
-						// gl_FragColor = vec4(
-						// 	normalize(vec3(
-						// 		random(vec2(seed,seed)),
-						// 		random(vec2(seed,seed)),
-						// 		random(vec2(seed,seed)))),
-						// 		1.0);
-
-						// float randval = random(vec2(0.1,float(seed*100.0)));
-						// float randval = 1.0;
-
-						// gl_FragColor = vec4(
-						// 	(vec3(
-						// 		random(vec2(seed,1.0)),
-						// 		0.0,
-						// 		0.0)),
-						// 		1.0);
-								
-						// gl_FragColor = vec4(faceNormal, 1.0);
-
 						break;
 
 					}
-
-					// 1 / PI attenuation for physically correct lambert model
-					// https://www.rorydriscoll.com/2009/01/25/energy-conservation-in-games/
-					// throughputColor = vec3( 1.0 );
-
-					randomPoint = vec3(
-						rand( vUv + float( i + 1 ) + vec2( seed, seed ) ),
-						rand( - vUv * seed + float( i ) - seed ),
-						rand( - vUv * float( i + 1 ) - vec2( seed, - seed ) )
-					);
-					randomPoint -= 0.5;
-					randomPoint *= 2.0;
-
-					// ensure the random vector is not 0,0,0 and that it won't exactly negate
-					// the surface normal
-
-					float pointLength = max( length( randomPoint ), 1e-4 );
-					randomPoint /= pointLength;
-					randomPoint *= 0.999;
-
-					// fetch the interpolated smooth normal
-					vec3 normal =
-						side *
-						textureSampleBarycoord(
-							normalAttribute,
-							barycoord,
-							faceIndices.xyz
-						).xyz;
 
 					// adjust the hit point by the surface normal by a factor of some offset and the
 					// maximum component-wise value of the current point to accommodate floating point
@@ -297,19 +213,10 @@ function init() {
 					vec3 point = rayOrigin + rayDirection * dist;
 					vec3 absPoint = abs( point );
 					float maxPoint = max( absPoint.x, max( absPoint.y, absPoint.z ) );
-					rayOrigin = point + faceNormal * ( maxPoint + 1.0 ) * RAY_OFFSET;
-		
-					
-					// rayDirection = faceNormal;
-					// normalize(vec3(1.0,1.0,0.0)); // normalize( normal + randomPoint );
-					// rayDirection = normalize(vec3(random(vec2(faceNormal.x,faceNormal.y)),random(vec2(faceNormal.x,faceNormal.z)),random(vec2(faceNormal.y,faceNormal.z))));
-					// rayDirection = (vec3(
-					// 	float(float(random(vec2(1.0, seed)) - 0.5)*2.0),
-					// 	float(float(random(vec2(2.0, seed)) - 0.5)*2.0),
-					// 	float(float(random(vec2(3.0, seed)) - 0.5)*2.0) ));
+					rayOrigin = point + faceNormal * ( maxPoint ) * RAY_OFFSET;
 
-					// rayDirection = viewFactorDirectionGenerator(faceNormal, vec2(seed, 2.0));
-					rayDirection = viewFactorDirectionGenerator(faceNormal, vec2(seed, abs(point.x * point.y * point.z)));
+					// VIEW FACTOR RAY DIRECTION GENERATION
+					rayDirection = normalize(viewFactorDirectionGenerator(faceNormal, vec2(seed, abs(point.x + point.y + point.z))));
 					// rayDirection = viewFactorDirectionGenerator(faceNormal, vec2(seed, abs(faceNormal.x + faceNormal.y + faceNormal.z)));
 				}
 
